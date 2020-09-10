1c325254c8f2dea93ef2199ffc49811b5be99976
DylanShadduck <dshadducksoccer@gmail.com>
2020-09-09 19:49:55 -0700

    upload center seeker bot

--- picobot_1.0/picobot.py
+++ picobot_1.0/picobot.py
@@ -5,11 +5,14 @@ from routines import *
 
 # This file is for strategy
 
-class ExampleBot(GoslingAgent):
-    def run(agent):
-        # An example of using raw utilities:
-        relative_target = agent.ball.location - agent.me.location
-        local_target = agent.me.local(relative_target)
-        defaultPD(agent, local_target)
-        defaultThrottle(agent, 2300)
-        default
+class Bully(GoslingAgent):
+    def run(self):
+        # This bot will be a bully and only try to boom other players
+        # when they are on their own half
+
+        relative_target = self.ball.location - self.me.location
+        local_target = self.me.local(relative_target)
+        defaultPD(self, local_target)
+        defaultThrottle(self, 2300)
+
+    def check_half(self):
