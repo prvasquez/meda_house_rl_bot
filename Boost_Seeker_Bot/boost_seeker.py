cc7ab541b1a5edaedfdcfcb72ef0161392188d4d
DylanShadduck <dshadducksoccer@gmail.com>
2020-09-10 18:13:35 -0700

    changed self to agent. Maybe that will help

--- Boost_Seeker_Bot/boost_seeker.py
+++ Boost_Seeker_Bot/boost_seeker.py
@@ -8,14 +8,14 @@ from routines import *
 # This file is for strategy
 
 class Boost_Seeker(GoslingAgent):
-    def run(self):
+    def run(agent):
         # this bot grabs boost
 
         # Checks the list of all boosts if they are active
         # picks the first boost it finds that is active and sets that as its target
-        if self.kickoff_flag:
-            self.push(kickoff())
+        if agent.kickoff_flag:
+            agent.push(kickoff())
         else:
-            boost = self.boosts[0]
-            self.push(goto_boost(self, boost))
+            boost = agent.boosts[0]
+            agent.push(goto_boost(agent, boost))
 
